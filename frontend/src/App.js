ce38b852b2eb488739514d945e169ed62d5bc11d
Kishan <bang.siki2009@gmail.com>
2023-01-25 18:34:20 +0545

    Added route for login & signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
@@ -11,6 +13,12 @@ function App() {
         <div className='pages'>
           <Routes>
             <Route path='/' element={<Home />} />
+          </Routes>
+          <Routes>
+            <Route path='/login' element={<Login />} />
+          </Routes>
+          <Routes>
+            <Route path='/signup' element={<Signup />} />
           </Routes>
         </div>
       </BrowserRouter>
